580ba1b311b2b638814a22b10ff4e64d1cd95b49
Philip Toan Nguyen <Philip2an@hotmail.com>
2023-03-20 00:52:11 +0100

    Startet grunnleggende. Lagt til bilder og tekst

---
+++ src/Gallery.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Img1 from './img/img1.jpg';
+import Img2 from './img/img2.jpg';
+import Img3 from './img/img3.jpg';
+import Img4 from './img/img4.jpg';
+
+const Gallery = () => {
+
+    let data = [{
+    
+        id: 1,
+        imgSrc: Img1,
+    },
+    {
+        id: 2,
+        imgSrc: Img2,
+    },
+    {
+        id: 3,
+        imgSrc: Img3,
+    },
+    {
+        id: 4,
+        imgSrc: Img4,
+    }
+]
+    return (
+        <>
+    
+        <div className="gallery">
+            {data.map((item, index)=>{
+              return(
+                <div className="pics" key={index}>
+                    <img src={item.imgSrc} style={{width: '100%'}} />  
+                </div>
+              )  
+            })}
+        </div>
+
+    
+       
+       </>
+    )
+}
+
+
+export default Gallery
